38f4597a8c9cc9868218c93aa98c8c50eba0af9d
kuzkoa <38584429+kuzkoa@users.noreply.github.com>
2019-05-23 21:43:00 +0200

    Jeszcze raz tytuł
    tym razem faktycznie zmienione, BreakerEasy jeszcze do poprawki

--- KTLV2/Strategy.cpp
+++ KTLV2/Strategy.cpp
@@ -6,9 +6,9 @@ int Maker::ChooseNode(vector<vector<Node*>>& H, Gameboard* gb)
 	double max = 0;
 	int v = -1;
 	int i = 0;
-	while (v < 0 && i < X.size())
+	while (i < X.size())
 	{
-		if (X[i].potential >= max && X[i].colour == 0) // && X[i].visited == false) jest juz niepotrzebne
+		if (X[i].potential >= max && X[i].colour == 0 ) //&& X[i].visited == false) //jest juz niepotrzebne
 		{
 			max = X[i].potential;
 			v = i;
@@ -55,7 +55,7 @@ int BreakerEasy::ChooseNode(vector<vector<Node*>>& hyp, Gameboard* gb)
 	Node* w = gb->findNodeWithValue(gb->LastMove()); //Node ostatniego ruchu
 	int i = 0;
 	int v = 0;
-	while (v != 0)			//zawsze musi byc jakis nie wybrany wierzcholke w ciagu, inaczej wygrana Makera
+	while (v = 0)			//zawsze musi byc jakis nie wybrany wierzcholke w ciagu, inaczej wygrana Makera
 	{
 		int seqIndex = w->degree[i];
 		for (int j = 0; j < hyp[seqIndex].size(); j++)
@@ -91,7 +91,7 @@ int BreakerHard::ChooseNode(vector<vector<Node*>>& H, Gameboard* gb)
 	double max = 0;
 	int v = -1;
 	int i = 0;
-	while (v < 0 && i < X.size())
+	while (i < X.size())
 	{
 		if (X[i].potential >= max && X[i].colour == 0) //jezeli niewybrany wierzcholek ma wiekszy potencjal niz max
 		{
@@ -116,11 +116,12 @@ void BreakerHard::ChangePotential(Node& moveMade, vector<vector<Node*>>& hypergr
 	{
 		for (int i = 0; i < moveMade.degree.size(); ++i)
 		{
-			for (int j = 0; j < hypergraph[i].size(); j++)
+			vector<Node*> nodes = hypergraph[moveMade.degree[i]];
+			for (int j = 0; j < hypergraph[moveMade.degree[i]].size(); j++)
 			{
 				//cout << "Before getting nodes for i = " << i << endl;
 				//cout << "Degree[i] = " << moveMade.degree[i] << endl;
-				vector<Node*> nodes = hypergraph[moveMade.degree[i]];
+				
 				//cout << "Got nodes!" << endl;
 				nodes[j]->potential *= 0;  //wiercholek wybral Maker-zwiekszamy potencjal
 			}
